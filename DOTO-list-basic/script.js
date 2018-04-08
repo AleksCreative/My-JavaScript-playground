513149cce0883b21396b1ce0035719b1e5045a3b
Aleks.Creative <aleksdesign@yahoo.co.uk>
2018-04-08 10:34:36 +0100

    tidies up the html and css code removing unwanted code and adds comments to js file

--- DOTO-list-basic/script.js
+++ DOTO-list-basic/script.js
@@ -1,30 +1,35 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+// Declaring variables
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
 
+// Function that determines the value of the text in the input box
 function inputLength() {
 	return input.value.length;
 }
 
+// Function that creates the list elements that contains text based on the input.value
 function createListElement() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
 }
 
+// Function that calls the createListElement() function after user clicks the button
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
+// Function that calls the createListElement() function after user presses the Enter key
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
 
+// Function calls on the events
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
