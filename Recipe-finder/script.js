e2a05e90cfd7ce81dd69cc662b28772a5c25e5fc
Aleks.Creative <aleksdesign@yahoo.co.uk>
2018-04-08 15:49:38 +0100

    unsuccessful attempt to create button ro remove all selected list items. code with checkboxes commented out

--- Recipe-finder/script.js
+++ Recipe-finder/script.js
@@ -13,15 +13,17 @@ function createListElement() {
 	let li = document.createElement("li");
 	li.setAttribute("class", "list-item");
 
+	/*
 	let tickBox = document.createElement("input");
 	tickBox.setAttribute("type", "checkbox");
 	tickBox.setAttribute("class", "tick-box")
+	*/
 
 	let clearButton = document.createElement("button");
 	clearButton.setAttribute("class", "clear-button");
 	clearButton.textContent = "Remove item";
 
-	li.appendChild(tickBox);
+	/* li.appendChild(tickBox); */
 	li.appendChild(document.createTextNode(input.value));
 	li.appendChild(clearButton);
 
@@ -48,3 +50,20 @@ function addListAfterKeypress(event) {
 // Function calls on the events
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
+
+// Creates Remove selected items button if the list is not empty - doesn't work!
+/*
+function addRemoveSelectedButton() {
+	if (ul.children > 0) {
+		let removeSelectedButton = document.createElement("button");
+		let selectedButtonContainer = document.getElementById("remove-selected");
+
+		removeSelectedButton.textContent = "Remove selected items";
+
+		selectedButtonContainer.appendChild(removeSelectedButton);
+		}
+}
+addRemoveSelectedButton();
+*/
+// Select the li elements from the document
+let listItems = ul.childNodes;
